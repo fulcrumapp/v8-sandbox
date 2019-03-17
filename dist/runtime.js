4891786e1c1246702a438a15b74d663ed0bc84b8
Zac McCormick <zac.mccormick@gmail.com>
2019-03-17 00:10:50 -0400

    Build

--- dist/runtime.js
+++ dist/runtime.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 global._tryCallback = func => {
   try {
     func();
@@ -25,7 +27,22 @@ global.dispatchSync = (name, args) => {
 
   const result = global._dispatchSync.apply(global, parameters);
 
-  return result != null ? JSON.parse(result) : null;
+  if (result != null) {
+    var _JSON$parse = JSON.parse(result),
+        _JSON$parse2 = _slicedToArray(_JSON$parse, 2);
+
+    const error = _JSON$parse2[0],
+          value = _JSON$parse2[1];
+
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return value;
+  }
+
+  return null;
 };
 
 global.dispatchAsync = (name, args, callback) => {
