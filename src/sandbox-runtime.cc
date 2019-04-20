6b5cbe314d9086ff73fe684acf098c64578e0ad5
Zac McCormick <zac.mccormick@gmail.com>
2019-04-20 00:15:47 -0400

    Update runtime

--- src/sandbox-runtime.cc
+++ src/sandbox-runtime.cc
@@ -21,7 +21,7 @@ global._execute = () => {
 };
 
 global.dispatchSync = (name, args) => {
-  const parameters = [ JSON.stringify([ name, ...args ]) ];
+  const parameters = [ JSON.stringify([ name, ...(args || []) ]) ];
 
   const result = global._dispatchSync.apply(global, parameters);
 
@@ -43,7 +43,7 @@ global.dispatchAsync = (name, args, callback) => {
     callback = () => {};
   }
 
-  const parameters = [ JSON.stringify([ name, ...args ]) ];
+  const parameters = [ JSON.stringify([ name, ...(args || []) ]) ];
 
   const wrappedCallback = (args) => {
     global._tryCallback(() => {
