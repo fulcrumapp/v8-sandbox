ddd5005f63d836f7d159fa428db38445d39682d7
Zac McCormick <zac.mccormick@gmail.com>
2020-03-21 10:34:35 -0400

    Fix stress

--- stress.js
+++ stress.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const REQUIRE = path.join(__dirname, 'test', 'test-functions.js');
 
-const sandbox = new Sandbox({require: REQUIRE});
+const sandbox = new Sandbox({ require: REQUIRE });
 
 const code = `
 let counter = 0;
@@ -15,21 +15,23 @@ for (let i = 0; i < 50000; ++i) {
   setTimeout(() => {
     counter++;
 
-    setResult({ value: { result: addNumbers(1, 2), count: counter } });
+    if (counter === 50000) {
+      setResult({ value: { result: addNumbers(1, 2), count: counter } });
+    }
   }, randomTimeout());
 }
 `;
 
 (async function() {
-  const { value, error, output } = await sandbox.execute({code, timeout: 300000});
+  const { value, error, output } = await sandbox.execute({ code, timeout: 300000 });
 
   if (error) {
     console.log('ERROR', error);
   }
-  
+
   assert.equal(value.result, 3);
-  assert.equal(value.count, 5000);
-  
+  assert.equal(value.count, 50000);
+
   sandbox.shutdown();
-})();
+}());
 
