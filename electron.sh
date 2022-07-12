23784c2f4b589f69e7bd7d5515023f44bb35d993
Zac McCormick <zac.mccormick@gmail.com>
2022-07-11 23:32:24 -0400

    Remove unused script

--- electron.sh
+++
@@ -1,7 +0,0 @@
-export npm_config_target="1.6.6"
-export npm_config_arch="x64"
-export npm_config_target_arch="x64"
-export npm_config_disturl="https://atom.io/download/electron"
-export npm_config_runtime="electron"
-export npm_config_build_from_source=true
-HOME=~/.electron-gyp yarn install --build-from-source
